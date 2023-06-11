d7d933102ea8ce1dabd655992292b0fb7aced653
Santiago Rincon Martinez <rincon.santi@gmail.com>
2023-06-11 13:55:56 +0200

    Fix changes

--- conversation_starter/main.py
+++ conversation_starter/main.py
@@ -1,7 +1,10 @@
 import firebase_admin
 import functions_framework
 from firebase_admin import firestore
+from flask import make_response
 import os
+import json
+import uuid
 
 # Get the project ID from the environment variables
 PROJECT_ID = os.environ.get('PROJECT_ID')
@@ -14,22 +17,27 @@ def conversation_start(request):
     This function starts a conversation.
     Parameters:
         request: The HTTP request.
-    """  
+    """
+    # Pre-flight request. Reply successfully:
+    response = make_response()
+    response.headers.set('Access-Control-Allow-Origin', '*')  # Adjust this to allow specific origins
+    response.headers.set('Access-Control-Allow-Methods', 'POST')
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.set('Access-Control-Max-Age', '3600')
+
     if request.method == 'OPTIONS':
-        logging.info("Received options request")
-        # Preflight request. Reply successfully:
-        response = make_response()
-        response.headers.set('Access-Control-Allow-Origin', '*')  # Adjust this to allow specific origins
-        response.headers.set('Access-Control-Allow-Methods', 'POST')
-        response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        response.headers.set('Access-Control-Max-Age', '3600')
+        print("Received options request")
         return response
+
     # Get the conversation ID from the request body
-    conversation_id = request.json.get('conversation_id')
+    conversation_id = str(request.json.get('conversationId'))
+    if not conversation_id:  # If the conversation ID is empty
+        conversation_id = str(uuid.uuid4())  # Create a new one
+
     # Get the model codename from the request body
     model_codename = "gpt-3.5-turbo"
     # Get the user ID from the request body if exists
-    user = request.json.get('user')
+    user = str(request.json.get('user'))
 
     # Create a new conversation document
     conversation_ref = firestore.client().collection(u'conversations').document(conversation_id)
@@ -39,5 +47,7 @@ def conversation_start(request):
         u'messages': []
     })
 
-    # Return a success response
-    return 'Conversation started successfully'
+    # Set the conversation ID in the response
+    response.data = json.dumps({'message': 'Conversation started successfully', 'conversationId': conversation_id})
+    response.headers.set('Content-Type', 'application/json')  # Make sure to set the content type to JSON
+    return response
