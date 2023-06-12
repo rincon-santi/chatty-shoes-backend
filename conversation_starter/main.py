3e8e8af6e2d6e3800091d1d07d4b138d090eb781
Santiago Rincon Martinez <rincon.santi@gmail.com>
2023-06-12 09:59:33 +0200

    Add language dependence on user country when creating conversation

--- conversation_starter/main.py
+++ conversation_starter/main.py
@@ -5,11 +5,15 @@ from flask import make_response
 import os
 import json
 import uuid
+import openai
 
 # Get the project ID from the environment variables
 PROJECT_ID = os.environ.get('PROJECT_ID')
 # Initialize the Firebase app
 APP = firebase_admin.initialize_app()
+# Set the OpenAI API key
+API_KEY = os.environ.get('OPENAI_KEY')
+openai.api_key = API_KEY
 
 @functions_framework.http
 def conversation_start(request):
@@ -38,16 +42,37 @@ def conversation_start(request):
     model_codename = "gpt-3.5-turbo"
     # Get the user ID from the request body if exists
     user = str(request.json.get('user'))
+    
+    country_code = str(request.json.get('user', 'UK'))
+    # Prepare prompt for language model
+    messages = [
+        {"role": "system", "content": "Act as a language identifier. You will be prompted with a country code and you will answer greeting the user in the corresponding language"},
+        {"role":"user", "content": country_code}
+    ]
+
+    # Get the greeting from the language model
+    full_prompt = {
+        "model": model_codename,
+        "messages": messages
+    }
+    response_prompt = openai.ChatCompletion.create(**full_prompt)
+    greeting = response_prompt['choices'][0]['message']['content']
+
+    initial_msg = {
+        'role': 'assistant',
+        'author': 'conversation-builder',
+        'content': greeting
+    }
 
     # Create a new conversation document
     conversation_ref = firestore.client().collection(u'conversations').document(conversation_id)
     conversation_ref.set({
         u'model_codename': model_codename,
         u'user': user,
-        u'messages': []
+        u'messages': [initial_msg]
     })
 
     # Set the conversation ID in the response
-    response.data = json.dumps({'message': 'Conversation started successfully', 'conversationId': conversation_id})
+    response.data = json.dumps({'message': initial_msg['content'], 'conversationId': conversation_id})
     response.headers.set('Content-Type', 'application/json')  # Make sure to set the content type to JSON
     return response
